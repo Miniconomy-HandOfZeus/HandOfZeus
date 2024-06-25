030df1eab3b959601dbf2a673ca59de0c98f578f
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-06-25 14:04:32 +0200

    Syntax changes

--- EventLambdas/DailyBroadcast/Function.cs
+++ EventLambdas/DailyBroadcast/Function.cs
@@ -7,36 +7,38 @@ using Amazon.SQS.Model;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace DailyBroadcast;
-
-public class Function
+namespace DailyBroadcast
 {
+
+  public class Function
+  {
     private static readonly AmazonDynamoDBClient _dynamoDbClient = new();
     private static readonly AmazonSQSClient _sqsClient = new();
     private static readonly string tableName = "ZeusTable";
-    private static readonly List<string> queueUrls = ["https://sqs.eu-west-1.amazonaws.com/427519652828/SimulationTimeQueue.fifo"]; // TODO: Enter all relevant queues to push the event to
+    private static readonly List<string> queueUrls = { "https://sqs.eu-west-1.amazonaws.com/427519652828/SimulationTimeQueue.fifo" }; // TODO: Enter all relevant queues to push the event to
 
     private static async Task<string> GetCurrentSimulationTime()
     {
-        // Get real-world simulation start time from DB
-        var key = new Dictionary<string, AttributeValue>
+      // Get real-world simulation start time from DB
+      var key = new Dictionary<string, AttributeValue>
         {
             { "Key", new AttributeValue { S = "SimulationStartTime" } }
         };
-        var request = new GetItemRequest
-        {
-            TableName = tableName,
-            Key = key
-        };
-        var response = await _dynamoDbClient.GetItemAsync(request);
+      var request = new GetItemRequest
+      {
+        TableName = tableName,
+        Key = key
+      };
+      var response = await _dynamoDbClient.GetItemAsync(request);
 
-        // Calculate the current simulation time
-        if (response.Item != null && response.Item.TryGetValue("Date", out AttributeValue? value))
-        {
-            var simulationStartTimeString = value.S;
-            var simulationStartTime = DateTime.ParseExact(simulationStartTimeString, "yyyy-MM-ddTHH:mm:ss", null, System.Globalization.DateTimeStyles.None);
-            var currentTime = DateTime.Now;
+      // Calculate the current simulation time
+      if (response.Item != null && response.Item.TryGetValue("Date", out AttributeValue? value))
+      {
+        var simulationStartTimeString = value.S;
+        var simulationStartTime = DateTime.ParseExact(simulationStartTimeString, "yyyy-MM-ddTHH:mm:ss", null, System.Globalization.DateTimeStyles.None);
+        var currentTime = DateTime.Now;
 
+<<<<<<< Updated upstream
             // Get the current day of the simulation (eg: day 1302)
             var simulationDayNumber = ((currentTime - simulationStartTime).TotalSeconds / 120) + 1;
 
@@ -57,36 +59,46 @@ public class Function
         {
             throw new Exception("Failed to get the current simulation time");
         }
+=======
+        var currentSimulationTime = DateTime.MinValue.AddSeconds((currentTime - simulationStartTime).TotalSeconds * 720);
+        return currentSimulationTime.ToString("yyyy-MM-dd");
+      }
+      else
+      {
+        throw new Exception("Failed to get the current simulation time");
+      }
+>>>>>>> Stashed changes
     }
 
     private static async Task SendEventToQueues(string messageBody)
     {
-        foreach (var queueUrl in queueUrls)
+      foreach (var queueUrl in queueUrls)
+      {
+        var sendMessageRequest = new SendMessageRequest
         {
-            var sendMessageRequest = new SendMessageRequest
-            {
-                QueueUrl = queueUrl,
-                MessageBody = messageBody,
-                MessageGroupId = "DailyBroadcastGroup"
-            };
-            await _sqsClient.SendMessageAsync(sendMessageRequest);
-        }
+          QueueUrl = queueUrl,
+          MessageBody = messageBody,
+          MessageGroupId = "DailyBroadcastGroup"
+        };
+        await _sqsClient.SendMessageAsync(sendMessageRequest);
+      }
     }
 
     public static async Task<string> FunctionHandler() // This is the function that gets executed every two minutes
     {
-        try
-        {
-            var messageBody = await GetCurrentSimulationTime();
-            await SendEventToQueues(messageBody);
+      try
+      {
+        var messageBody = await GetCurrentSimulationTime();
+        await SendEventToQueues(messageBody);
 
 
-            return "Message sent to SQS queues.";
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-            throw;
-        }
+        return "Message sent to SQS queues.";
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error: {ex.Message}");
+        throw;
+      }
     }
+  }
 }
