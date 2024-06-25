2b8b7a2aba97dd8335b1782a381106e663821c6d
Janro Botha <Janro.Botha@bbd.co.za>
2024-06-25 22:26:10 +0200

    basline Random Event Lambda

--- EventLambdas/RandomEvent/RandomEvent/Function.cs
+++ EventLambdas/RandomEvent/RandomEvent/Function.cs
@@ -1,5 +1,11 @@
 using Amazon.Lambda.Core;
-
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -7,15 +13,30 @@ namespace RandomEvent;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+
+  private static readonly Random random = new Random();
+  private static readonly List<string> events = new List<string>
+  {
+    "Death",
+    "Marriage",
+    "Birth",
+    "Hunger",
+    "Sickness",
+    "Breakages",
+    "Salary",
+    "Fired from job",
+    "Famine",
+    "Plague",
+    "War",
+    "Apocalypse",
+    "Inflation"
+  };
+
+
+  public string FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        var selectedEvent = events[random.Next(events.Count)];
+        context.Logger.LogLine($"Selected Event : {selectedEvent}");
+        return selectedEvent;
     }
 }
