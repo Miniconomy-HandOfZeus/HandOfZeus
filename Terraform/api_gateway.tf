da6177f4b4b6167cfdd88813e8d49e5df214dd4d
Michael Rolle <michael.rolle@bbd.co.za>
2024-06-26 21:45:54 +0200

    Removed allow credentials

--- Terraform/api_gateway.tf
+++ Terraform/api_gateway.tf
@@ -4,7 +4,6 @@ resource "aws_apigatewayv2_api" "api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_credentials = true
     allow_headers = ["*"]
     allow_methods = ["*"]
     allow_origins = ["*"]
