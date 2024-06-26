3a17fc8e700a5e4db2d8533e029f2015ed3b4fa0
Michael Rolle <michael.rolle@bbd.co.za>
2024-06-26 19:43:15 +0200

    Attached TLS cert to api gateway

--- Terraform/api_gateway.tf
+++ Terraform/api_gateway.tf
@@ -1,12 +1,28 @@
-resource "aws_apigatewayv2_api" "service_endpoints" {
+resource "aws_apigatewayv2_api" "api" {
   name          = "${var.naming_prefix}-service-api"
   description   = "API Gateway for the service endpoints"
   protocol_type = "HTTP"
 }
 
+resource "aws_apigatewayv2_domain_name" "api" {
+  domain_name = "api.zeus.projects.bbdgrad.com"
+
+  domain_name_configuration {
+    certificate_arn = "arn:aws:acm:us-east-1:625366111301:certificate/584d9cbb-d67d-4acf-aa23-5b4faaa99ca2"
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "api" {
+  api_id      = aws_apigatewayv2_api.api.id
+  domain_name = aws_apigatewayv2_domain_name.api.domain_name
+  stage       = "$default"
+}
+
 resource "aws_apigatewayv2_integration" "lambda_integrations" {
   for_each         = { for index, config in var.lambda_endpoint_config : index => config }
-  api_id           = aws_apigatewayv2_api.service_endpoints.id
+  api_id           = aws_apigatewayv2_api.api.id
   integration_type = "AWS_PROXY"
 
   connection_type    = "INTERNET"
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "backend_endpoint" {
-  value = aws_apigatewayv2_api.service_endpoints.api_endpoint
+  value = aws_apigatewayv2_api.api.api_endpoint
 }
 
 output "frontend_endpoint" {
