60b10bcf86498199a3a671c8d163718dd8499b80
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-06-29 17:38:44 +0200

    start/reset endpoint

---
+++ EndpointLambdas/StartOrResetSim/Services/RequestHandler.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using StartOrResetSim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartOrResetSim.Services
+{
+    public class RequestHandler : IRequestHandler
+    {
+        private readonly HttpClient client;
+
+        public RequestHandler()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<bool> SendPutRequestAsync(string url, bool value, string startTime)
+        {
+            // Prepare the query parameter based on the boolean value
+            string param = value ? "start" : "reset";
+
+            // Build the request URL with the query parameter
+            string requestUrl = $"{url}?action={param}";
+
+            try
+            {
+                HttpContent content = null;
+
+                if (value)
+                {
+                    var requestBody = new { startTime };
+                    var json = JsonConvert.SerializeObject(requestBody);
+                    content = new StringContent(json, Encoding.UTF8, "application/json");
+                }
+
+                
+                var response = await client.PutAsync(requestUrl, content);
+
+                
+                response.EnsureSuccessStatusCode();
+
+                return true; // Return true to indicate success
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error sending PUT request: {e.Message}");
+                return false; // Return false if there was an error
+            }
+        }
+    }
+}
