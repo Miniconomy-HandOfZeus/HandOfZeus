8c1c99443778ea5668952a0f9304f6bde6f21718
Joseph-Kimathi <joseph.kimathi@bbd.co.za>
2024-06-29 14:13:00 +0200

    Revert "Chagned to dotnet 8, I hope"

--- EndpointLambdas/MinimumWage/Function.cs
+++ EndpointLambdas/MinimumWage/Function.cs
@@ -13,103 +13,49 @@ using Amazon.DynamoDBv2.Model;
 
 namespace MinimumWage
 {
-  public class Function
-  {
-    private static readonly AmazonDynamoDBClient _dynamoDbClient = new AmazonDynamoDBClient();
-    private static readonly string tableName = "hand-of-zeus-db";
-
-    public static APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    public class Function
     {
-      var response = new APIGatewayProxyResponse
-      {
-        StatusCode = 200,
-        Body = JsonSerializer.Serialize(new { message = input.Body }),
-        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-      };
-
-      Function function = new Function();
-      string date = "01|01|01";
-      string ans = function.getWage(date);
-      return response;
-    }
-    private string getWage(string date)
-    {
-      if (semiAnnual(date))
-      {
-        generateWage();
-      }
-      return fetchFromDB("minimum_wage").Result;
-    }
-
-    private bool semiAnnual(string date)
-    {
-      string[] dateSplit = date.Split('|');
-
-      return (dateSplit[1].Equals("01") && dateSplit[2].Equals("01")) || (dateSplit[1].Equals("06") && dateSplit[2].Equals("01"));
-    }
-
-    private void generateWage()
-    {
-      Random randomSeed = new Random();
-      int seed = randomSeed.Next(int.MinValue, int.MaxValue);
-      Random random = new Random(seed);
-      pushDB("minimum_wage", random.Next(10, 30));
-      return;
-    }
-    public static async Task<string> grabKey()
-    {
-      var key = new Dictionary<string, AttributeValue>
+        private static readonly AmazonDynamoDBClient _dynamoDbClient = new AmazonDynamoDBClient();
+        private static readonly string tableName = "ZeusTable";
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+        {
+            var task = Function.grabKey();
+            System.Console.WriteLine(task.Id + " " + task.ToString() + " " + task.Result);
+            var response = new APIGatewayProxyResponse
+            {
+              StatusCode = 200,
+              Body = JsonSerializer.Serialize(new { message = input.Body }),
+              Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+            return "test";
+        }
+        public static async Task<string> grabKey()
+        {
+            var key = new Dictionary<string, AttributeValue>
         {
             { "Key", new AttributeValue { S = "minimum_wage" } }
         };
-      GetItemRequest request = new GetItemRequest
-      {
-        TableName = tableName,
-        Key = key
-      };
-      try
-      {
-        var response = await _dynamoDbClient.GetItemAsync(request);
-        System.Console.WriteLine(response.ToString(), response.Item);
-        return response.ToString();
-      }
-      catch
-      {
-        return null;
-      }
+            GetItemRequest request = new GetItemRequest
+            {
+                TableName = tableName,
+                Key = key
+            };
+            try
+            {
+                var response = await _dynamoDbClient.GetItemAsync(request);
+                System.Console.WriteLine(response.ToString(), response.Item);
+                return response.ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
-
-    private void pushDB(string key, object value)
-    {
-      Dictionary<string, AttributeValue> item = new Dictionary<string, AttributeValue>
-        {
-            { "Key", new AttributeValue { S = key } },
-            { "Value", new AttributeValue { S = JsonSerializer.Serialize(new { value = value }) } }
-        };
-
-    }
-    private async static Task<string> fetchFromDB(string key)
-    {
-      var dbRequest = new GetItemRequest
-      {
-        TableName = "hand-of-zeus",
-        Key = new Dictionary<string, AttributeValue>
-                {
-                    { "Key", new AttributeValue { S = key } }
-                }
-      };
-      try
-      {
-        var response = await _dynamoDbClient.GetItemAsync(dbRequest);
-        System.Console.WriteLine(response.ToString(), response.Item);
-        return response.Item[key].S;
-      }
-      catch (Exception e)
-      {
-        throw e;
-      }
-      string value = "";
-      return value;
-    }
-  }
 }
