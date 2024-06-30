1eb5ecc69d7e08ef4d88325fad68c01eb93da5ec
Michael Rolle <michael.rolle@bbd.co.za>
2024-06-30 14:41:21 +0200

    Finished calculate date function

--- EndpointLambdas/DateCalculation/Function.cs
+++ EndpointLambdas/DateCalculation/Function.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
@@ -12,29 +12,104 @@ using Amazon.Lambda.Core;
 
 namespace DateCalculation
 {
-  public class Function
-  {
-    public static APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    public class Function
     {
-      var response = new APIGatewayProxyResponse
-      {
-        StatusCode = 200,
-        Body = JsonSerializer.Serialize(new { message = input.Body }),
-        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-      };
-      string date = "01|01|01";
-      string calculation = calculateDate(date);
-      return response;
-    }
-    private static string calculateDate(string date)
-    {
-      string[] splitDate = date.Split('|');
+        private readonly static string tableName = "hand-of-zeus-db";
+        private static readonly AmazonDynamoDBClient _dynamoDbClient = new();
 
-      //Heebie Jeebie Black Magic Box Calc
+        public static async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
 
-      string ans = "";
-      return ans;
+            
+            long systemTimeMilliseconds;
+
+            if (input.QueryStringParameters.TryGetValue("time", out string systemTimeMillisecondsString)) 
+            {
+                if (long.TryParse(systemTimeMillisecondsString, out systemTimeMilliseconds)) 
+                {
+                    if (systemTimeMilliseconds < 0)
+                    {
+                        response.StatusCode = 400;
+                        response.Body = "Query parameter 'time' must be a positive integer";
+                        return response;
+                    }
+                }
+                else
+                {
+                    response.StatusCode = 400;
+                    response.Body = "The 'time' query parameter must be an integer";
+                    return response;
+                }
+            }
+            else
+            {
+                response.StatusCode = 400;
+                response.Body = "Missing required query parameter 'time'";
+                return response;
+            }
+
+            DateTime simulationStartDate = await getSimulationStartDate();
+            DateTime currentDate = DateTimeOffset.FromUnixTimeMilliseconds(systemTimeMilliseconds).LocalDateTime;
+
+            string date = calculateDate(simulationStartDate, currentDate);
+            response.Body = JsonSerializer.Serialize(new
+            {
+                date
+            });
+            
+            return response;
+        }
+
+        private static async Task<DateTime> getSimulationStartDate()
+        {
+            // Get real-world simulation start time from DB
+            var key = new Dictionary<string, AttributeValue>
+            {
+                { "Key", new AttributeValue { S = "SimulationStartTime" } }
+            };
+            var request = new GetItemRequest
+            {
+                TableName = tableName,
+                Key = key
+            };
+            var response = await _dynamoDbClient.GetItemAsync(request);
+
+            if (response.Item != null && response.Item.TryGetValue("Value", out AttributeValue? value))
+            {
+                string simulationStartTimeString = value.S;
+                DateTime simulationStartTime = DateTime.ParseExact(simulationStartTimeString, "yyyy-MM-ddTHH:mm:ss", null, System.Globalization.DateTimeStyles.None);
+
+                return simulationStartTime;
+            }
+            else
+            {
+                throw new Exception("Unable to retrieve the simulation start time from the db");
+            }
+        }
+
+        private static string calculateDate(DateTime simulationStartDate, DateTime currentDate)
+        {
+            // Get the current day of the simulation (eg: day 1302)
+            int simulationDayNumber = (int) Math.Floor(((currentDate - simulationStartDate).TotalSeconds / 120) + 1);
+
+            // Calculate current year
+            int year = (int) Math.Floor((double) (simulationDayNumber / 360)) + 1;
+            int daysIntoYear = (int) Math.Floor((double) (simulationDayNumber % 360));
+
+            // Calculate current month and day
+            int month = (int) Math.Floor((double) (daysIntoYear / 30)) + 1;
+            int day = (int) Math.Floor((double) daysIntoYear % 30);
+
+            // Format the year, month, and day with leading zeros
+            string formattedDate = $"{year:00}|{month:00}|{day:00}";
+
+            return formattedDate;
+        }
     }
-  }
 }
 
