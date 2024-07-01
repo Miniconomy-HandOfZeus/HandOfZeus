1edae6b5cbfc6221293919f189f11b86088af386
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-07-01 15:16:04 +0200

    test request

--- EndpointLambdas/GettingCertTest/Function.cs
+++ EndpointLambdas/GettingCertTest/Function.cs
@@ -55,10 +55,21 @@ public class Function
                 }
             }
 
+
+            var baseUri = "https://api.zeus.projects.bbdgrad.com/date"; // Replace with your actual API endpoint
+            var timeQueryParam = "1719837257315"; // Use an appropriate format for your time parameter
+
+            // Construct the full URI with the query parameter
+            var uriBuilder = new UriBuilder(baseUri);
+            var query = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["time"] = timeQueryParam;
+            uriBuilder.Query = query.ToString();
+            var requestUri = uriBuilder.ToString();
+
             // Create an HttpClient using the handler
             using (var httpClient = new HttpClient(handler))
             {
-                var requestUri = "https://api.zeus.projects.bbdgrad.com/food-price"; // Replace with your actual API endpoint
+                //var requestUri = "https://api.zeus.projects.bbdgrad.com/date"; // Replace with your actual API endpoint
                 var content = new StringContent("{\"key\":\"value\"}", System.Text.Encoding.UTF8, "application/json"); // Replace with your actual payload
 
                 // Make the PUT request
