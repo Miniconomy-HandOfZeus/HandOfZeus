7577a3e753c771fba42dfeec00f9cd039c70bba5
Michael Rolle <michael.rolle@bbd.co.za>
2024-07-02 23:19:41 +0200

    Formatted

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -39,7 +39,7 @@ variable "service_lambda_endpoint_config" {
     method            = string
     description       = string
     lambda_invoke_arn = string
-    db_key  = string
+    db_key            = string
     allowed_services  = list(string)
   }))
   description = "The configuration for the integration of Lambda functions into API gateway."
