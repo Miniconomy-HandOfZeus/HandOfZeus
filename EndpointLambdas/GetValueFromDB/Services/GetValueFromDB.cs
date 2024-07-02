86e74936e242d479765626cea6ebd8036a3d88c8
Michael Rolle <michael.rolle@bbd.co.za>
2024-07-02 22:51:45 +0200

    Basic reusable get lambda done

---
+++ EndpointLambdas/GetValueFromDB/Services/GetValueFromDB.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace GetValueFromDB.Services
+{
+    public class Repository
+    {
+        private static readonly string tableName = "hand-of-zeus-db";
+        private readonly AmazonDynamoDBClient client;
+
+        public Repository()
+        {
+            client = new AmazonDynamoDBClient();
+        }
+
+        public async Task<int> GetValue(string key)
+        {
+            var request = new GetItemRequest
+            {
+                TableName = tableName,
+                Key = new Dictionary<string, AttributeValue>
+            {
+                { "Key", new AttributeValue { S = key } }
+            }
+            };
+
+            try
+            {
+                var response = await client.GetItemAsync(request);
+
+                if (response.Item == null || !response.Item.ContainsKey("value"))
+                {
+                    throw new Exception($"{key} not found in the db.");
+                }
+
+                return int.Parse(response.Item["value"].N);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+
+        }
+
+    }
+}
