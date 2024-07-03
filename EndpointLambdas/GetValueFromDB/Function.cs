834c2b5c7631c70e8a52705565c466bcd3a4a88e
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-07-03 15:47:01 +0200

    removing MTLS

--- EndpointLambdas/GetValueFromDB/Function.cs
+++ EndpointLambdas/GetValueFromDB/Function.cs
@@ -10,67 +10,31 @@ namespace GetValueFromDB;
 
 public class Function
 {
-    private readonly Repository db = new();
+  private readonly Repository db = new();
 
-    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+  public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+  {
+    var queryParams = input.QueryStringParameters;
+    if (queryParams == null || !queryParams.ContainsKey("service"))
     {
-        if (input.QueryStringParameters == null)
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 500,
-                Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
-
-        // Parse the custom query parameters set by API gateway
-        if (!input.QueryStringParameters.TryGetValue("allowed_services", out string? allowedServicesString) || !input.QueryStringParameters.TryGetValue("key", out string? key))
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 500,
-                Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
-        List<string> allowedServices = [.. allowedServicesString.Split(",")];
-
-        context.Logger.Log($"Allowed services: {string.Join(", ", allowedServices) }");
-        context.Logger.Log($"DB key: {key}");
-
-        // Validate calling service
-        if (input.RequestContext.Authorizer.TryGetValue("clientCertCN", out var callingServiceObject))
-        {
-            string callingService = callingServiceObject?.ToString() ?? string.Empty;
-            context.Logger.Log($"{callingService} requested the price");
-            if (!allowedServices.Contains(callingService))
-            {
-                return new APIGatewayProxyResponse
-                {
-                    StatusCode = 403,
-                    Body = JsonConvert.SerializeObject(new { message = "Forbidden service" }),
-                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-                };
-            }
-        }
-        else
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 403,
-                Body = JsonConvert.SerializeObject(new { message = "Forbidden" }),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
-
-        int value = await db.GetValue(key);
-
-        return new APIGatewayProxyResponse
-        {
-            StatusCode = 200,
-            Body = JsonConvert.SerializeObject(new { value }),
-            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-        };
+      return new APIGatewayProxyResponse
+      {
+        StatusCode = 500,
+        Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
+        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+      };
     }
+
+    context.Logger.Log($"requested the price");
+    string key = queryParams["service"];
+    int value = await db.GetValue(key);
+
+    return new APIGatewayProxyResponse
+    {
+      StatusCode = 200,
+      Body = JsonConvert.SerializeObject(new { value }),
+      Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+    };
+  }
 }
+
--- EndpointLambdas/SickPersonaDies/Function.cs
+++ EndpointLambdas/SickPersonaDies/Function.cs
@@ -20,56 +20,6 @@ public class Function
 
     public async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest input, ILambdaContext context)
     {
-        if (input.QueryStringParameters == null)
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 500,
-                Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
-
-        // Parse the custom query parameters set by API gateway
-        if (!input.QueryStringParameters.TryGetValue("allowed_services", out string? allowedServicesString) || !input.QueryStringParameters.TryGetValue("key", out string? key))
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 500,
-                Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
-        List<string> allowedServices = [.. allowedServicesString.Split(",")];
-
-        context.Logger.Log($"Allowed services: {string.Join(", ", allowedServices)}");
-        context.Logger.Log($"DB key: {key}");
-
-        // Validate calling service
-        if (input.RequestContext.Authorizer.TryGetValue("clientCertCN", out var callingServiceObject))
-        {
-            string callingService = callingServiceObject?.ToString() ?? string.Empty;
-            context.Logger.Log($"{callingService} called this endpoint!");
-            if (!allowedServices.Contains(callingService))
-            {
-                return new APIGatewayProxyResponse
-                {
-                    StatusCode = 403,
-                    Body = JsonConvert.SerializeObject(new { message = "Forbidden service" }),
-                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-                };
-            }
-        }
-        else
-        {
-            return new APIGatewayProxyResponse
-            {
-                StatusCode = 403,
-                Body = JsonConvert.SerializeObject(new { message = "Forbidden" }),
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
-
         // Parse the request body to get the person ID
         var requestBody = JsonConvert.DeserializeObject<Dictionary<string, BigInteger>>(input.Body);
         if (requestBody == null || !requestBody.ContainsKey("personaId"))
@@ -94,8 +44,8 @@ public class Function
 
         if (!response.survives)
         {
-            X509Certificate2 certs = await CertHandler.GetCertAndKey();
-            await RequestHandler.SendPostRequestAsync(certs, response);
+            //X509Certificate2 certs = await CertHandler.GetCertAndKey();
+            await RequestHandler.SendPostRequestAsync(response);
         }
 
         return new APIGatewayProxyResponse
