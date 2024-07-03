1888be8088f01e296aff139c5f39789ad27dd145
Michael Rolle <michael.rolle@bbd.co.za>
2024-07-03 15:46:08 +0200

    Removed unnecessary code

--- EventLambdas/MontlyIncreases/Function.cs
+++ EventLambdas/MontlyIncreases/Function.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2;
@@ -105,12 +104,6 @@ namespace MontlyIncreases
       {
         throw e;
       }
-
-      long newPopulation = await ;
-      double rate = 1/ oldPopulation;
-
-
-      //assessRate(rate);
     }
   }
 }
