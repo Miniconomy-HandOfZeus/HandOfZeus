6901615e9f3fdfc0d841f3e16963430761b87c25
SamanthaVS-BBD <157207389+SamanthaVS-BBD@users.noreply.github.com>
2024-07-03 14:07:51 +0200

    try, catch and date stuff (#90)

--- EndpointLambdas/StartOrResetSim/Services/DeterminePrice.cs
+++ EndpointLambdas/StartOrResetSim/Services/DeterminePrice.cs
@@ -14,13 +14,13 @@ namespace StartOrResetSim.Services
 
         List<Item> prices = new List<Item>
 {
-            new Item { Name = "health_insurance", Price = 300 },
-            new Item { Name = "prime_lending_rate", Price = 200 },
+            new Item { Name = "health_insurance", Price = 100 },
+            new Item { Name = "prime_lending_rate", Price = 100 },
             new Item { Name = "taxes", Price = 400 },
             new Item { Name = "food_price", Price = 400 },
             new Item { Name = "life_insurance", Price = 400 },
             new Item { Name = "short_term_insurance", Price = 400 },
-            new Item { Name = "minimum_wage", Price = 400 },
+            new Item { Name = "minimum_wage", Price = 600 },
             new Item { Name = "eletronic_price", Price = 400 },
             new Item { Name = "house_price", Price = 400 }
         };
@@ -40,7 +40,7 @@ namespace StartOrResetSim.Services
             });
         }
 
-        public async Task setStartTime(string key, DateTime startTime)
+        public async Task setStartTime(string key, string startTime)
         {
             DBHelper.SetInDbString(key, startTime.ToString());
         }
