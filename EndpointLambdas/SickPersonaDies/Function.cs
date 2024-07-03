60dba6d03535078b8ee59aa4bd3bb39d4fac4867
Michael Rolle <michael.rolle@bbd.co.za>
2024-07-03 08:31:07 +0200

    Added mTLS service validation to sick persona endpoint

--- EndpointLambdas/SickPersonaDies/Function.cs
+++ EndpointLambdas/SickPersonaDies/Function.cs
@@ -11,10 +11,60 @@ public class Function
 {
     private static readonly Random random = new Random();
 
-    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
+        if (input.QueryStringParameters == null)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 500,
+                Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+
+        // Parse the custom query parameters set by API gateway
+        if (!input.QueryStringParameters.TryGetValue("allowed_services", out string? allowedServicesString) || !input.QueryStringParameters.TryGetValue("key", out string? key))
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 500,
+                Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+        List<string> allowedServices = [.. allowedServicesString.Split(",")];
+
+        context.Logger.Log($"Allowed services: {string.Join(", ", allowedServices)}");
+        context.Logger.Log($"DB key: {key}");
+
+        // Validate calling service
+        if (input.RequestContext.Authorizer.TryGetValue("clientCertCN", out var callingServiceObject))
+        {
+            string callingService = callingServiceObject?.ToString() ?? string.Empty;
+            context.Logger.Log($"{callingService} called this endpoint!");
+            if (!allowedServices.Contains(callingService))
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 403,
+                    Body = JsonConvert.SerializeObject(new { message = "Forbidden service" }),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+                };
+            }
+        }
+        else
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 403,
+                Body = JsonConvert.SerializeObject(new { message = "Forbidden" }),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+
         // Parse the request body to get the person ID
-        var requestBody = JsonConvert.DeserializeObject<Dictionary<string, string>>(request.Body);
+        var requestBody = JsonConvert.DeserializeObject<Dictionary<string, string>>(input.Body);
         if (requestBody == null || !requestBody.ContainsKey("personaId"))
         {
             return new APIGatewayProxyResponse
@@ -33,8 +83,8 @@ public class Function
         // Create the response
         var response = new
         {
-            personId = personId,
-            survives = survives
+            personId,
+            survives
         };
 
         return new APIGatewayProxyResponse
