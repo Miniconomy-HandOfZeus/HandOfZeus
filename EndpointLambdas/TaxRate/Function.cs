93d11eced138dca6a1e0347d4fec3c03afdd76e7
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-07-03 11:44:54 +0200

    precursonr for taxes

--- EndpointLambdas/TaxRate/Function.cs
+++ EndpointLambdas/TaxRate/Function.cs
@@ -19,6 +19,55 @@ namespace TaxRate
 
     public static APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
+      if (input.QueryStringParameters == null)
+      {
+        return new APIGatewayProxyResponse
+        {
+          StatusCode = 500,
+          Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
+          Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+      }
+
+      // Parse the custom query parameters set by API gateway
+      if (!input.QueryStringParameters.TryGetValue("allowed_services", out string? allowedServicesString) || !input.QueryStringParameters.TryGetValue("key", out string? key))
+      {
+        return new APIGatewayProxyResponse
+        {
+          StatusCode = 500,
+          Body = JsonConvert.SerializeObject(new { message = "Internal server error" }),
+          Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+      }
+      List<string> allowedServices = [..allowedServicesString.Split(",")];
+
+      context.Logger.Log($"Allowed services: {string.Join(", ", allowedServices) }");
+      context.Logger.Log($"DB key: {key}");
+
+      // Validate calling service
+      if (input.RequestContext.Authorizer.TryGetValue("clientCertCN", out var callingServiceObject))
+      {
+        string callingService = callingServiceObject?.ToString() ?? string.Empty;
+        context.Logger.Log($"{callingService} requested the price");
+        if (!allowedServices.Contains(callingService))
+        {
+          return new APIGatewayProxyResponse
+          {
+            StatusCode = 403,
+            Body = JsonConvert.SerializeObject(new { message = "Forbidden service" }),
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+          };
+        }
+      }
+      else
+      {
+        return new APIGatewayProxyResponse
+        {
+          StatusCode = 403,
+          Body = JsonConvert.SerializeObject(new { message = "Forbidden" }),
+          Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+      }
       string[] rate = new Function().getRate("");
       var response = new APIGatewayProxyResponse
       {
