3c73edcfed4c5898d1c14a713faa2c44f65c5ec2
Michael Rolle <michael.rolle@bbd.co.za>
2024-07-04 10:09:38 +0200

    Updated request handler

--- EndpointLambdas/StartOrResetSim/Services/RequestHandler.cs
+++ EndpointLambdas/StartOrResetSim/Services/RequestHandler.cs
@@ -1,68 +1,49 @@
 ﻿using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 using StartOrResetSim.Interfaces;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace StartOrResetSim.Services
 {
     public class RequestHandler : IRequestHandler
     {
-        private readonly HttpClientHandler client;
+        private readonly HttpClient httpClient;
 
         public RequestHandler()
         {
-           client = new HttpClientHandler();
+            httpClient = new HttpClient();
         }
 
         public async Task<bool> SendPutRequestAsync(string url, bool value, string startTime)
         {
-            //client.ClientCertificates.Add(cert);
             // Prepare the query parameter based on the boolean value
             string action = value ? "start" : "reset";
 
-            using (var httpClient = new HttpClient(client))
+            try
             {
-                try
+                HttpContent content = null;
+
+                var requestBody = new
                 {
-                    HttpContent content = null;
+                    action = action,
+                    startTime = value ? startTime : null
+                };
 
-                    if (value)
-                    {
-                        var requestBody = new
-                        {
-                            action = action,
-                            startTime = startTime
-                        };
+                var json = JsonConvert.SerializeObject(requestBody);
+                content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                        var json = JsonConvert.SerializeObject(requestBody);
-                        content = new StringContent(json, Encoding.UTF8, "application/json");
-                    }
-                    else
-                    {
-                        var requestBody = new
-                        {
-                            action = action,
-                        };
+                var response = await httpClient.PostAsync(url, content);
+                LambdaLogger.Log(response.ToString());
 
-                        var json = JsonConvert.SerializeObject(requestBody);
-                        content = new StringContent(json, Encoding.UTF8, "application/json");
-                    }
+                response.EnsureSuccessStatusCode();
 
-                    var response = await httpClient.PostAsync(url, content);
-                    LambdaLogger.Log(response.ToString());
-
-                    response.EnsureSuccessStatusCode();
-
-                    return true; // Return true to indicate success
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine($"Error sending PUT request: {e.Message}");
-                    return false; // Return false if there was an error
-                }
+                return true; // Return true to indicate success
             }
-                
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error sending PUT request: {e.Message}");
+                return false; // Return false if there was an error
+            }
         }
     }
 }
